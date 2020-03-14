da792313d87812123c2d5ded15733adc1edf41e0
@gigaprakosa <mamikosjakarta02@mamikoss-MacBook-Pro.local>
2020-03-14 14:22:31 +0700

    Pilot example for using Laravel & Vue in Front-end

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+require('./bootstrap'); //import main library
+
+require('bulma'); //import Bulma for CSS framework
+
+window.Vue = require('vue'); //import Vue for JS framework
+
+import home from './components/Home.vue'; //import & define
+
+const app = new Vue({
+	components: {
+		home //register
+	}
+}).$mount('#app');
