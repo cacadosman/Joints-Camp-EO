816c5fb9d24e9fc6eab91368dfa05bf850806623
Wikan Kuncara Jati <wikan25@gmail.com>
2020-05-10 00:31:41 +0700

    Update 2020_05_09_140044_create_proposals_table.php

--- database/migrations/2020_05_09_140044_create_proposals_table.php
+++ database/migrations/2020_05_09_140044_create_proposals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProposalTable extends Migration
+class CreateProposalsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -30,6 +30,6 @@ class CreateProposalTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('proposal');
+        Schema::dropIfExists('proposals');
     }
 }
